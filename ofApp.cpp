b49c95e9cd27e9d065285d37f4cf46095e2a9df5
Feature Creature <jdema003@gold.ac.uk>
2019-07-31 00:11:48 +0700

    update and draw eeg data

--- ofApp.cpp
+++ ofApp.cpp
@@ -9,23 +9,50 @@ void ofApp::setup(){
     receiver.setup(PORT);
     ofLog() << "Listening for OSC messages on port " << PORT;
 
+    bufferSize = 1280;
+    tp9.assign(bufferSize,0.0);
+    af7.assign(bufferSize,0.0);
+    af8.assign(bufferSize,0.0);
+    tp10.assign(bufferSize,0.0);
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     // clear old data
+    /*
     for(int i = 0; i < MAX_NUM_MSG_STRINGS; i++){
         if(timers[i] < ofGetElapsedTimef()){
             msgStrings[i] = "";
         }
     }
+    */
 
     while(receiver.hasWaitingMessages()){
         ofxOscMessage m;
         receiver.getNextMessage(m);
 
-        // if(m.getAddress() == ""){}
-        
+        // 220Hz || 256Hz
+        if(m.getAddress() == "/muse/eeg"){
+
+            updateEEG(m,tp9,0);
+            updateEEG(m,af7,1);
+            updateEEG(m,af8,2);
+            updateEEG(m,tp10,3);
+        }
+
+        // 10Hz
+        if(m.getAddress() == "/muse/elements/horseshoe"){
+            updateEEGStatus(m,tp9Status,0);
+            updateEEGStatus(m,af7Status,1);
+            updateEEGStatus(m,af8Status,2);
+            updateEEGStatus(m,tp10Status,3);
+        }
+
+    }
+
+        //basic text output 
+        /*
         string msgString;
         msgString = m.getAddress();
         msgString += ":";
@@ -41,9 +68,6 @@ void ofApp::update(){
             else if(m.getArgType(i) == OFXOSC_TYPE_FLOAT){
                 msgString += ofToString(m.getArgAsFloat(i));
             }
-            else if(m.getArgType(i) == OFXOSC_TYPE_STRING){
-                msgString += ofToString(m.getArgAsString(i));
-            }
             else {
                 msgString += "unhandled argument type " + m.getArgTypeName(i);
             }
@@ -55,19 +79,58 @@ void ofApp::update(){
 
         msgStrings[currentMsgString] = "";
     }
+    */
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
     ofBackgroundGradient(100,0);
 
+    /*
     for(int i = 0; i < MAX_NUM_MSG_STRINGS; i++){
         ofDrawBitmapStringHighlight(msgStrings[i],10,40 + 15 * i);
     }
+    */
+
+    drawEEG(tp9,tp9Status);
+    drawEEG(af7,af7Status);
+    drawEEG(af8,af8Status);
+    drawEEG(tp10,tp10Status);
 
     string buf = "listening for osc messages on port " + ofToString(PORT);
     ofDrawBitmapStringHighlight(buf,10,20);
+}
+
+//--------------------------------------------------------------
+void ofApp::updateEEG(ofxOscMessage msg, vector<float>& sensor, int msgNum){
+    sensor.push_back(msg.getArgAsFloat(msgNum));
+    if(sensor.size() >= bufferSize)sensor.erase(sensor.begin(),sensor.begin()+1);
+}
+
+//--------------------------------------------------------------
+void ofApp::updateEEGStatus(ofxOscMessage msg, vector<float>& status, int msgNum){
+    status.push_back(msg.getArgAsFloat(msgNum));
+    if(status.size() >= bufferSize)status.erase(status.begin(),status.begin()+1);
+}
+
+//--------------------------------------------------------------
+void ofApp::drawEEG(vector <float> sensor,vector <float> status){
+    ofPushStyle();
+    ofPushMatrix();
+
+    ofTranslate(32,600,0);
+
+    ofNoFill();
+    status.size() > 0 ? ofSetColor(ofMap(status[status.size() - 1],4,1,100,255)) : ofSetColor(100);
+
+    ofBeginShape();
+    for(unsigned int i = 0; i < sensor.size(); i++){
+        ofVertex(i*(ofGetWidth()/bufferSize),ofMap(sensor[i],0,1682.815,0,-500));
+    }
+    ofEndShape(false);
+
+    ofPopMatrix();
+    ofPopStyle();
 }
 
 //--------------------------------------------------------------
--- ofApp.h
+++ ofApp.h
@@ -24,6 +24,9 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+        void drawEEG(vector<float> sensor,vector<float> status);
+        void updateEEG(ofxOscMessage msg, vector<float>& sensor,int msgNum);
+        void updateEEGStatus(ofxOscMessage msg, vector<float>& status, int msgNum);
 
         ofxOscReceiver receiver;
         ofTrueTypeFont font;
@@ -31,5 +34,18 @@ class ofApp : public ofBaseApp{
         int currentMsgString;
         string msgStrings[MAX_NUM_MSG_STRINGS];
         float timers[MAX_NUM_MSG_STRINGS];
+        int bufferSize; 
+
+        // EEG
+        vector <float> tp9;
+        vector <float> af7;
+        vector <float> af8;
+        vector <float> tp10;
+
+        // EEG status
+        vector <float> tp9Status;
+        vector <float> af7Status;
+        vector <float> af8Status;
+        vector <float> tp10Status;
 
 };

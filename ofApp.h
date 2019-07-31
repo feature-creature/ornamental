37931d356956cf734cfa86f70c9e872c97b27df9
Feature Creature <jdema003@gold.ac.uk>
2019-07-31 14:36:20 +0100

    update and draw absolutes data

--- ofApp.h
+++ ofApp.h
@@ -24,17 +24,16 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-        void drawEEG(vector<float> sensor,vector<float> status);
+
         void updateEEG(ofxOscMessage msg, vector<float>& sensor,int msgNum);
         void updateEEGStatus(ofxOscMessage msg, vector<float>& status, int msgNum);
+        void drawEEG(vector<float> sensor,vector<float> status);
+
+        void updateAbsolute(ofxOscMessage msg, vector<float>& sensor);
+        void drawAbsolute(vector<float> sensor,vector<float> status);
 
         ofxOscReceiver receiver;
         ofTrueTypeFont font;
-
-        int currentMsgString;
-        string msgStrings[MAX_NUM_MSG_STRINGS];
-        float timers[MAX_NUM_MSG_STRINGS];
-        int bufferSize; 
 
         // EEG
         vector <float> tp9;
@@ -47,5 +46,17 @@ class ofApp : public ofBaseApp{
         vector <float> af7Status;
         vector <float> af8Status;
         vector <float> tp10Status;
+    
+        // Absolutes
+        vector <float> alpha;
+        vector <float> beta;
+        vector <float> delta;
+        vector <float> theta;
+        vector <float> gamma;
 
+
+        int currentMsgString;
+        string msgStrings[MAX_NUM_MSG_STRINGS];
+        float timers[MAX_NUM_MSG_STRINGS];
+        int bufferSize; 
 };
